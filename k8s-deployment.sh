af703fa577217bec8dfefbeddb7c9154e26819f1
Angel <agarciamal1997@gmail.com>
2022-09-26 11:18:45 +0200

    promote prod

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -16,4 +16,3 @@ sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
 
 
 kubectl -n default apply -f k8s_deployment_service.yaml
-kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
